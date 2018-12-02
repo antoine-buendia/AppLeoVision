127525ccc7b5efa01ccba3a862be7fab5eb95d22
Antoine <fquispebuendia@gmail.com>
2018-12-02 13:04:04 -0500

    cambios basicos

--- ProyectoAppAndroid/app/src/main/java/com/example/frank/leovision/MainActivity.java
+++ ProyectoAppAndroid/app/src/main/java/com/example/frank/leovision/MainActivity.java
@@ -12,6 +12,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -22,6 +23,13 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+
+        CharSequence text = "Bienvenido!";
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(this, text, duration);
+        toast.show();
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
