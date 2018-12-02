802e1df65eaabdc8a065906cf9c39f060e46774d
antoine <fquispebuendia@gmail.com>
2018-12-01 22:05:55 -0500

    agregando fragmentos

--- ProyectoAppAndroid/app/src/main/java/com/example/frank/leovision/MainActivity.java
+++ ProyectoAppAndroid/app/src/main/java/com/example/frank/leovision/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.frank.leovision;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,9 +18,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.example.frank.leovision.activities.fragment.ProductsFragment;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+        FragmentManager fragmentManager;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +34,14 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        CharSequence text = "YYYYYYY!";
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(this, text, duration);
+        toast.show();
+
+
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -81,8 +101,19 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         if (id == R.id.nav_camera) {
+
+
+            CharSequence text = "Productos!";
+            int duration = Toast.LENGTH_SHORT;
+            Toast toast = Toast.makeText(this, text, duration);
+            toast.show();
+
+            ChangeActivity();
+
             // Handle the camera action
         } else if (id == R.id.nav_gallery) {
+
+
 
         } else if (id == R.id.nav_slideshow) {
 
@@ -98,4 +129,22 @@ public class MainActivity extends AppCompatActivity
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
+    public void ChangeActivity( ) {
+//        Intent intent = new Intent(this, ProductsFragment.class);
+//
+//        startActivity(intent);
+        // First get FragmentManager object.
+        fragmentManager = this.getSupportFragmentManager();
+
+        // Begin Fragment transaction.
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        // Replace the layout holder with the required Fragment object.
+        fragmentTransaction.replace(R.id.dynamic_fragment_frame_layout,  new ProductsFragment());
+
+        // Commit the Fragment replace action.
+        fragmentTransaction.commit();
+    }
+
 }
--- ProyectoAppAndroid/app/src/main/java/com/example/frank/leovision/activities/ContainerActivity.java
+++ ProyectoAppAndroid/app/src/main/java/com/example/frank/leovision/activities/ContainerActivity.java
@@ -2,6 +2,7 @@ package com.example.frank.leovision.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.example.frank.leovision.R;
 
@@ -11,5 +12,11 @@ public class ContainerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_container);
+
+        CharSequence text = "XXXXXXXXXXXXXXXXXXX!";
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(this, text, duration);
+        toast.show();
+
     }
 }
